034ac814299ab28fa034ac0ef4082481d3e958c8
Mohit Kishor Chaudhari <mohit.chaudhary.freelancer@gmail.com>
2022-06-13 17:42:15 +0530

    added README.md

---
+++ Project Code/calculations.php
@@ -0,0 +1,12 @@
+
+<?php
+
+$name = $_POST['firstname'];
+$code_number = $_POST['code-number'];
+$Valuation_Date = $_POST['Valuation-Date'];
+$country = $_POST['country'];
+
+
+echo "your name is: ",$name;
+
+?>
